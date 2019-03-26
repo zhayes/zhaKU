c8a8bb866046369f31f36f2b3a8697f36ac45f6a
chad <zhayaping@outlook.com>
2019-03-26 13:25:19 +0800

    update

---
+++ deploy.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env sh
+
+# abort on errors
+set -e
+
+# build
+npm run docs:build
+
+git checkout gh-pages
+
+rm -rf asserts
+rm -rf components
+
+mv docs/.vuepress/dist/* .
+
+rm -rf docs
+
+git add -A
+git commit -m 'deploy zhakU press'
+
+git push
+
+echo'完成'
